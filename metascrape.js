2da8273d96574e3b62357e23decb9722aa036300
Jon Bloomer <jon@freaksauce.com>
2015-03-24 10:36:44 +0000

    switched to anonyfox:scrape to get page info

--- metascrape.js
+++ metascrape.js
@@ -1,36 +1,37 @@
 if (Meteor.isClient) {
- 
+  
   Template.metainfo.helpers({
-    name: function() {
-      return Session.get('name');
+    title: function() {
+      return Session.get('title');
     },
     description: function() {
       return Session.get('description');
     },
-    images: function() {
-      return Session.get('images');
+    image: function() {
+      return Session.get('image');
     }
   });
  
    Template.metascrape.events({
     'click #submit': function (evt) {
       var url = $('#url').val();
-      console.log(url);
-      Meteor.call('simplecrawler_findMetadata', url, function (error, result) {
-          if (error) {
-            console.log(error);
-          }else{         
-            console.log(result);
-            Session.set({
-              name: result.name,
-              description: result.description,
-              images: result.images              
-            });
-          }
-          //Error is empty if all goes well
-          //Result will containt and object like:
-          //{name:'Page name',description:'Here's the metadata description',images:['img url 1','img url 2']}
+//       console.log(url);
+      Meteor.call('scrapepage', url, function(error, result) {
+        if (error) {
+          console.log(error);
+        }else{
+          console.log(result);
+          Session.set({
+            title: result.title,
+            description: result.description,
+            image: result.image              
+          });
+          
+          console.log(gm.isAvailable);
+          
+        }
       });
+
       evt.preventDefault();
     }
   });
@@ -40,5 +41,13 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+  
+  Meteor.methods({
+    'scrapepage': function(pageUrl) {
+      console.log('scraping');
+      data = Scrape.website(pageUrl);
+      return data;
+    }
   });
 }
